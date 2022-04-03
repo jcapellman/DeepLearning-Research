8d634723244d882088fe1d74da6b25e158e5d758
Jarred Capellman <jcapellman@hotmail.com>
2022-04-03 09:59:25 -0500

    Added: Keras NuGet Package

--- Day2/Program.cs
+++ Day2/Program.cs
@@ -1,10 +1,15 @@
-﻿namespace Day2
+﻿using static Tensorflow.Binding;
+using static Tensorflow.KerasApi;
+using Tensorflow;
+using Tensorflow.NumPy;
+
+namespace Day2
 {
     class Day2
     {
         static void Main(string[] args)
         {
-
+            
         }
     }
 }
